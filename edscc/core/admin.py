8a389b0830659adc53a5cc4eb85e01278fe4cd68
dr. josh butler <josh.butler929@gmail.com>
2021-05-05 12:30:10 -0400

    Update admin.py to restrict modifying your own account - Add Sync Galnet Feed button to Galnet News

--- edscc/core/admin.py
+++ edscc/core/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import Carousel, CommunityGoal, GalnetNews, Motd
+
+# Unregister the provided model admin for User
+admin.site.unregister(User)
+
+
+# Register our own model admin, based on the default UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = [
+        'date_joined',
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        if not is_superuser and obj is not None and obj == request.user:
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
 
 
 # Register your models here.
@@ -20,6 +60,7 @@ class CommunityGoalAdmin(admin.ModelAdmin):
 
 class GalnetNewsAdmin(admin.ModelAdmin):
     list_display = ("title", "ed_date", "slug", "date_added")
+    change_list_template = "%s%s" % (settings.APPS_DIR, '/templates/admin/galnet_change_list.html')
 
 
 admin.site.register(Motd, MotdAdmin)
