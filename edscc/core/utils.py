f5f1da2587428357be0093b213524d394c6dda22
dr. josh butler <josh.butler929@gmail.com>
2021-05-15 22:40:51 -0400

    Add bulk create records

--- edscc/core/utils.py
+++ edscc/core/utils.py
@@ -2,9 +2,11 @@ import hashlib
 import json
 import logging
 import os.path
+from collections import defaultdict
 
 import filetype
 import requests
+from django.apps import apps
 from django.conf import settings
 from django.contrib import messages
 from django.utils.translation import gettext as _
@@ -15,6 +17,69 @@ from edscc.core.models import GalnetNews
 
 log = logging.getLogger(__name__)
 BUF_SIZE = 64 * 1024  # read in 64k chunks
+
+
+class BulkCreateManager(object):
+    """
+    This helper class keeps track of ORM objects to be created for multiple
+    model classes, and automatically creates those objects with `bulk_create`
+    when the number of objects accumulated for a given model class exceeds
+    `chunk_size`.
+    Upon completion of the loop that's `add()`ing objects, the developer must
+    call `done()` to ensure the final set of objects is created for all models.
+    """
+
+    def __init__(self, chunk_size=100):
+        self._create_queues = defaultdict(list)
+        self.chunk_size = chunk_size
+
+    def _commit(self, model_class):
+        model_key = model_class._meta.label
+        model_class.objects.bulk_create(self._create_queues[model_key])
+        self._create_queues[model_key] = []
+
+    def add(self, obj):
+        """
+        Add an object to the queue to be created, and call bulk_create if we
+        have enough objs.
+        """
+        model_class = type(obj)
+        model_key = model_class._meta.label
+        self._create_queues[model_key].append(obj)
+        if len(self._create_queues[model_key]) >= self.chunk_size:
+            self._commit(model_class)
+
+    def done(self):
+        """
+        Always call this upon completion to make sure the final partial chunk
+        is saved.
+        """
+        for model_name, objs in self._create_queues.items():
+            if len(objs) > 0:
+                self._commit(apps.get_model(model_name))
+
+
+def fetch_eddb_data(endpoint):
+    eddb_url = getattr(settings, "EDDB_URL")
+    endpoint_url = f"{eddb_url}{endpoint}"
+    headers = {"Accept-Encoding": "gzip, deflate, sdch"}
+    download_path = f"{settings.MEDIA_ROOT}/eddb/{endpoint}"
+    r = None
+    data = {}
+    try:
+        r = requests.get(endpoint_url, headers=headers)
+        r.raise_for_status()
+        status = {"Status": r.status_code}
+        open(download_path, "wb").write(r.content)
+    except requests.exceptions.Timeout as e:
+        status = {"Status": r.status_code, "Error": "%s" % e}
+    except requests.exceptions.ConnectionError as e:
+        status = {"Status": r.status_code, "Error": "%s" % e}
+    except requests.exceptions.HTTPError as e:
+        status = {"Status": r.status_code, "Error": "%s" % e}
+    except requests.exceptions.RequestException as e:
+        status = {"Status": r.status_code, "Error": "%s" % e}
+    return status, download_path
 
 
 def fetch_galnet_news_feed():
